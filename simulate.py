ea7a61e61279b69906cb7c9f995f9c404d6ac2ac
joseph la delfa <cafeciaojoe>
2024-03-11 15:21:28 +0100

    step 11
    render window opens and stays open for a while then closes,

--- simulate.py
+++ simulate.py
@@ -1,5 +1,12 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+
+# Let's slow things down so we can see our simulated world.
+# Between the connect and disconnect lines, include a for loop that iterates 1000 times.
+# Inside the loop include
+for i in range(1,100000):
+    p.stepSimulation()
 
 p.disconnect()

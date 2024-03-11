0f831358ec0180f27dec5bdae067256aca05124d
joseph la delfa <cafeciaojoe>
2024-03-11 15:27:10 +0100

    step 11
    added option to disable debug sidebars

--- simulate.py
+++ simulate.py
@@ -3,10 +3,16 @@ import time
 
 physicsClient = p.connect(p.GUI)
 
+# to disable the sidebars on the pybullet simulation.
+# This change will also dramatically speed up the GUI simulation on some platforms.
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
 # Let's slow things down so we can see our simulated world.
 # Between the connect and disconnect lines, include a for loop that iterates 1000 times.
 # Inside the loop include
-for i in range(1,100000):
+for i in range(1,1000):
     p.stepSimulation()
+    time.sleep(.016)
+    print(i)
 
 p.disconnect()

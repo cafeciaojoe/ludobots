57917b83ca6b638d0601b7defc3b5fb9d3ddc844
joseph la delfa <cafeciaojoe>
2024-05-23 05:16:21 +0200

    Step 69
    https://www.reddit.com/r/ludobots/wiki/refactoring/

--- simulation.py
+++ simulation.py
@@ -43,9 +43,8 @@ class SIMULATION:
         loops = c.loops
         for t in range(1, loops):
             #print(i)
-            timeStep = t
             p.stepSimulation()
-            self.robot.Sense(timeStep)
+            self.robot.Sense(t)
             # # print(backLegSensorValues[i],frontLegSensorValues[t])
             #
             # # During each step of the simulation, we are going to simulate a motor that supplies force to one of the robot's joints. To do so, add this statement

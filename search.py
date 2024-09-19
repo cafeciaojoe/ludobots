0b399d773a0b7b44581bfb6aeb15b56c3a946ab3
joseph la delfa <cafeciaojoe>
2024-09-19 05:25:59 +0200

    Step 8
    hill climber
    
    https://www.reddit.com/r/ludobots/wiki/randomsearch/

--- search.py
+++ search.py
@@ -1,7 +1,11 @@
 import os
+import hillclimber
+
+#create an instance of HILL_CLIMBER class called hc
+hc = hillclimber.HILL_CLIMBER()
 
 
-for i in range(0,5):
-    os.system("python3 generate.py")
-    os.system("python3 simulate.py")
+# for i in range(0,5):
+#     os.system("python3 generate.py")
+#     os.system("python3 simulate.py")
 
